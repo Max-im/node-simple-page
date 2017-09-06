6495506728d8f423f6c1efe9d37d09a019bb9401
Max Pozhidayev <pogidaevmo@gmail.com>
2017-09-06 20:37:53 +0300

    try node

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+
+hbs.registerPartials(__dirname+'/views/partials/');
+app.set('view engine', 	'hbs');
+
+
+app.use((req, res, next) => {
+	const now = new Date().toString();
+	const log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if(err) {
+			console.log('server error');
+		}
+	});
+	next();
+});
+
+
+// app.use( (req, res, next) => {
+// 	res.render('newFile.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+
+hbs.registerHelper('toUpCase', (text) => {
+	return text.toUpperCase();
+});
+
+
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcome: "welcome on this page"
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+
+
+app.listen( port, () => {
+	console.log('server run')
+})
