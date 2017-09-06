3f0c48ec4eb5e6bc6245467ef265d8af8e52956d
Max Pozhidayev <pogidaevmo@gmail.com>
2017-09-06 20:59:04 +0300

    add a new page

--- app.js
+++ app.js
@@ -56,6 +56,12 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'my awesome projects'
+	});
+});
+
 
 
 app.listen( port, () => {
